e15053ddd84feb624acfaf8e940274b11832f13d
trentonjavadroid1660 <59889828+trentonjavadroid1660@users.noreply.github.com>
2020-03-27 18:59:47 -0400

    Update Tasks.java

--- src/com/company/Greenlee/Tasks.java
+++ src/com/company/Greenlee/Tasks.java
@@ -34,6 +34,8 @@ public class Tasks {
 // Chained Comparators
 // Could not find a way to handle both different comparators in the one main class. Had to use two separate classes
 // that each could handle one of the comparators since it's possible to return only one statement in the compare method.
+
+// Class Title sorts things starting with A-Z(Uppercase) then a-z(Lowercase).
 class Title implements Comparator<Tasks> {
     @Override
     public int compare(Tasks o1, Tasks o2) {
@@ -41,6 +43,7 @@ class Title implements Comparator<Tasks> {
     }
 }
 
+// Class Priority reverse sorts priorities from greatest to lowest.
 class Priority implements Comparator<Tasks> {
     @Override
     public int compare(Tasks o1, Tasks o2) {
